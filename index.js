630231471f0da55b33eed221e4bef141f5aa39e1
Julian Clavijo <julianclavijo@Julians-iMac.local>
2022-07-07 16:35:08 -0500

    update

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const descuento = (valor , descuento) => {
+const descuento = (idProduct , valor , descuento ) => {
 
     const valoraDescontar = valor *  (descuento / 100);
     const valorFinal = valor - valoraDescontar;
 
-    return valorFinal;
+    console.log(valoraDescontar);
+    
+    const label = document.getElementById('product' + idProduct + '');
+    label.innerHTML = 'Precio con descuento : ' + '$' + valorFinal;
     
 }
+
